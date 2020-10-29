2e4c9c6e14eac4396a3d60eb028971d37f364ab3
jmgomez2018 <jmgametal@gmail.com>
2020-10-29 08:46:48 -0300

    Clase 32. Store en el componente hijo

--- 02-redux-app/src/app/contador/hijo/hijo.component.ts
+++ 02-redux-app/src/app/contador/hijo/hijo.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
 
 @Component({
   selector: 'app-hijo',
@@ -6,27 +8,22 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./hijo.component.scss']
 })
 export class HijoComponent implements OnInit {
-  @Input() contador: number;
-  @Output() changeCount = new EventEmitter<number>();
+  contador: number;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.select('contador').subscribe( contador => this.contador = contador);
   }
 
   onMultiply() {
-    this.contador *= 2;
-    this.changeCount.emit(this.contador);
+    // this.contador *= 2;
+    // this.changeCount.emit(this.contador);
   }
 
   onDivide() {
-    this.contador /= 2;
-    this.changeCount.emit(this.contador);
-  }
-
-  resetCount(count: number) {
-    this.contador = count;
-    this.changeCount.emit(this.contador);
+    // this.contador /= 2;
+    // this.changeCount.emit(this.contador);
   }
 
 }
