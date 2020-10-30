5dbb787f4a5464b811149388867a43939c3a6e4b
jmgomez2018 <jmgametal@gmail.com>
2020-10-30 10:49:57 -0300

    Clase 44. Agregar un TODO & 45. Desplegar nuestra lista de TODOS

--- 03-todo-app/src/app/app.module.ts
+++ 03-todo-app/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     ReactiveFormsModule,
     TodoModule,
-    StoreModule.forRoot({ todo: todoReducer }),
+    StoreModule.forRoot({ todos: todoReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
--- 03-todo-app/src/app/todos/todo.reducer.ts
+++ 03-todo-app/src/app/todos/todo.reducer.ts
@@ -3,7 +3,10 @@ import { Todo } from './models/todo.model';
 import { crear } from './todo.actions';
 
 export const initialState: Todo[] = [
-  new Todo('Salvar al mundo')
+  new Todo('Salvar al mundo'),
+  new Todo('Vencer a Thanos'),
+  new Todo('Coprar traje de Ironmab'),
+  new Todo('Robar escudo del Capitán América')
 ];
 
 const _todoReducer = createReducer(
