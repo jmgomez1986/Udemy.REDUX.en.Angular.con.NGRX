71054d92d0d173ab1def0b875ad2f5547ebda4f5
jmgomez2018 <jmgametal@gmail.com>
2020-11-02 11:19:08 -0300

    Clase 52. Combinando 2 o más reducers en el AppState

--- 03-todo-app/src/app/app.module.ts
+++ 03-todo-app/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 
 // NgRx
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { todoReducer } from './todos/todo.reducer';
+import { appReducers } from './app.reducer';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
 
@@ -21,7 +21,7 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     ReactiveFormsModule,
     TodoModule,
-    StoreModule.forRoot({ todos: todoReducer }),
+    StoreModule.forRoot( appReducers ),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
