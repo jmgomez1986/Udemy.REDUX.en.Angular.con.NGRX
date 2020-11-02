dc7d5a084080cd1cad2dafb14f9a6082bef9122e
jmgomez2018 <jmgametal@gmail.com>
2020-11-02 10:35:40 -0300

    Clase 51. Creando un segundo reducer - Filtro

---
+++ 03-todo-app/src/app/filtro/filtro.actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from '@ngrx/store';
+
+export type filtrosValidos = 'todos' | 'completados' | 'pendientes';
+
+export const setFilter = createAction(
+  '[Filter] Set Filter',
+  props<{ filtro: filtrosValidos }>()
+);
+
+
+
---
+++ 03-todo-app/src/app/filtro/filtro.reducer.ts
@@ -0,0 +1,14 @@
+import { createReducer, on } from '@ngrx/store';
+import { filtrosValidos, setFilter } from './filtro.actions';
+
+export const initialState: filtrosValidos = 'todos';
+
+const _filtroReducer = createReducer(
+  initialState,
+  on(setFilter, (state, {filtro}) => filtro),
+);
+
+export function filtroReducer(state, action) {
+  return _filtroReducer(state, action);
+}
+
