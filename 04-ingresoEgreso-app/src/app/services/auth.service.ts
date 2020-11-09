8292fce19bbb119347e065da7dfc2214eb374859
jmgomez2018 <jmgametal@gmail.com>
2020-11-09 17:21:50 -0300

    Clase 81. Crear documento de base de datos por cada usuario

--- 04-ingresoEgreso-app/src/app/services/auth.service.ts
+++ 04-ingresoEgreso-app/src/app/services/auth.service.ts
@@ -1,24 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(
+    public auth: AngularFireAuth,
+    private firestore: AngularFirestore) { }
 
   initAuthListener(): Observable<firebase.User> {
     return this.auth.authState;
   }
 
-  crearUsuario(nombre: string, correo: string, password: string): Promise<firebase.auth.UserCredential> {
+  crearUsuario(nombre: string, correo: string, password: string): Promise<any> {
     console.log({nombre, correo, password});
 
-    return firebase.auth().createUserWithEmailAndPassword(correo, password);
+    return firebase.auth().createUserWithEmailAndPassword(correo, password)
+      .then(({user}) => {
+        const newUser = new Usuario(user.uid, nombre, user.email);
+
+        return this.firestore.doc(`${user.uid}/usuario`)
+          .set({...newUser});
+      });
   }
 
   loginUsuario(correo: string, password: string): Promise<firebase.auth.UserCredential> {
