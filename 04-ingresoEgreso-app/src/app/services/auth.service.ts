7bda9568a9bf953a693bf654552a0a9a33b0ab30
jmgomez2018 <jmgametal@gmail.com>
2020-11-09 09:20:56 -0300

    Clase 79. Obtener información del usuario activo en Firebase

--- 04-ingresoEgreso-app/src/app/services/auth.service.ts
+++ 04-ingresoEgreso-app/src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
 
@@ -9,17 +10,21 @@ export class AuthService {
 
   constructor(public auth: AngularFireAuth) { }
 
-  crearUsuario(nombre: string, correo: string, password: string) {
+  initAuthListener(): Observable<firebase.User> {
+    return this.auth.authState;
+  }
+
+  crearUsuario(nombre: string, correo: string, password: string): Promise<firebase.auth.UserCredential> {
     console.log({nombre, correo, password});
 
     return firebase.auth().createUserWithEmailAndPassword(correo, password);
   }
 
-  loginUsuario(correo: string, password: string) {
+  loginUsuario(correo: string, password: string): Promise<firebase.auth.UserCredential> {
     return firebase.auth().signInWithEmailAndPassword(correo, password);
   }
 
-  logout() {
+  logout(): Promise<void> {
     return firebase.auth().signOut();
   }
 }
