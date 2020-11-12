0e893010d637f87de7cad7e61eb398b6f154869e
jmgomez2018 <jmgametal@gmail.com>
2020-11-12 08:45:16 -0300

    Clase 100. Optimizaciones de loading

--- 04-ingresoEgreso-app/src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ 04-ingresoEgreso-app/src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,27 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import Swal from 'sweetalert2';
+import { AppState } from '../app.reducer';
 import { IngresoEgreso } from '../models/ingreso-egreso.model';
 import { IngresoEgresoService } from '../services/ingreso-egreso.service';
+import * as uiActions from '../shared/ui.actions';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
   styleUrls: ['./ingreso-egreso.component.scss']
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
   ingresoForm: FormGroup;
   tipo = 'ingreso';
+  cargando = false;
+  loadingSubs: Subscription;
 
   constructor(
     private fb: FormBuilder,
-    private ingresoEgresoService: IngresoEgresoService) { }
+    private ingresoEgresoService: IngresoEgresoService,
+    private store: Store<AppState>) { }
 
   ngOnInit() {
     this.ingresoForm = this.fb.group({
       descripcion: ['', Validators.required],
       monto: ['', Validators.required],
     });
+
+    this.loadingSubs = this.store.select('ui').subscribe(ui => this.cargando = ui.isLoading);
+  }
+
+  ngOnDestroy(): void {
+    this.loadingSubs.unsubscribe();
   }
 
   guardar() {
@@ -29,17 +42,23 @@ export class IngresoEgresoComponent implements OnInit {
       return;
     }
 
-    const {descripcion, monto} = this.ingresoForm.value;
+    this.store.dispatch(uiActions.isLoading());
+
+    const { descripcion, monto } = this.ingresoForm.value;
 
     const ingresoEgreso = new IngresoEgreso(descripcion, monto, this.tipo);
 
     this.ingresoEgresoService.crearIngresoEgreso(ingresoEgreso)
-    .then(ref => {
-      console.log('Exito: ', ref);
-      this.ingresoForm.reset();
-      Swal.fire('Registro creado', descripcion, 'success');
-    })
-    .catch(err => Swal.fire('Error: ', err.messege, 'error'));
+      .then(ref => {
+        console.log('Exito: ', ref);
+        this.ingresoForm.reset();
+        this.store.dispatch(uiActions.stopLoading());
+        Swal.fire('Registro creado', descripcion, 'success');
+      })
+      .catch(err => {
+        this.store.dispatch(uiActions.stopLoading());
+        Swal.fire('Error: ', err.messege, 'error');
+      });
   }
 
 }
