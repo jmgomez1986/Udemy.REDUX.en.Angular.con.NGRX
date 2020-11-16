6c673b192719fc74aaa4db8f30c6937631f857e3
jmgomez2018 <jmgametal@gmail.com>
2020-11-16 20:38:50 -0300

    Clase 121. CanLoad

--- 04-ingresoEgreso-app/src/app/app-routing.module.ts
+++ 04-ingresoEgreso-app/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-// import { AuthGuardService } from './auth/auth-guard.service';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
@@ -13,8 +13,8 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   {
     path: '',
+    canLoad: [ AuthGuard ],
     loadChildren: './ingreso-egreso/ingreso-egreso.module#IngresoEgresoModule',
-    // canLoad: [ AuthGuardService ]
   },
   { path: '**', redirectTo: '' },
 
