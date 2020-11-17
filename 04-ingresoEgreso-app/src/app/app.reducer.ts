e0dac27b23f41ce9026b41cca0952de1d23abe03
jmgomez2018 <jmgametal@gmail.com>
2020-11-16 21:46:19 -0300

    Se elimina codigo comentado

--- 04-ingresoEgreso-app/src/app/app.reducer.ts
+++ 04-ingresoEgreso-app/src/app/app.reducer.ts
@@ -1,16 +1,12 @@
 import { ActionReducerMap } from '@ngrx/store';
 import * as ui from './shared/ui.reducers';
 import * as auth from './auth/auth.reducers';
-// import * as ingresoEgreso from './ingreso-egreso/ingreso-egreso.reducers';
-
 export interface AppState {
   ui: ui.State;
   user: auth.State;
-  // ingresosEgresos: ingresoEgresos.State;
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
   ui: ui.uiReducer,
   user: auth.authReducer,
-  // ingresosEgresos: ingresoEgresos.ingresosEgresosReducer,
 };
