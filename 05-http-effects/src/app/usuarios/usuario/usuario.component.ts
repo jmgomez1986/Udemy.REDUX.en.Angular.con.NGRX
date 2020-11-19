87e4c12100262a6ac0268245af55398838dd6e33
jmgomez2018 <jmgametal@gmail.com>
2020-11-19 18:06:14 -0300

    Clase 154. Mostrar la información del usuario

--- 05-http-effects/src/app/usuarios/usuario/usuario.component.ts
+++ 05-http-effects/src/app/usuarios/usuario/usuario.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { Usuario } from 'src/app/models/usuario.model';
 import { cargarUsuario } from 'src/app/store/actions';
 import { AppState } from 'src/app/store/app.reducers';
 
@@ -11,15 +13,25 @@ import { AppState } from 'src/app/store/app.reducers';
 })
 export class UsuarioComponent implements OnInit {
   id: number;
+  user: Usuario;
+  loading = false;
+  error: any;
+  subscriptions: Array<Subscription>;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.select('usuario')
+      .subscribe(({user, loading, error}) => {
+        this.user = user;
+        this.loading = loading;
+        this.error = error;
+      });
+
     this.activatedRoute.params.subscribe(params => {
       this.id = params.id;
-      console.log('ID: ', this.id);
       this.store.dispatch(cargarUsuario({id: this.id}));
     });
   }
